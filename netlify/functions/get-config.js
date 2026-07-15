cf656798b3aaf5116905472972adeb80c2f6feec
om pradip patil <omspradippatil2007@gmail.com>
2026-07-15 14:55:17 +0530

    feat: integrate Firebase, capture payer details, and add Top Supporters leaderboard

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -12,9 +12,22 @@ exports.handler = async () => {
     };
   }
 
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ key_id: keyId }),
+    body: JSON.stringify({ 
+      key_id: keyId,
+      firebaseConfig: firebaseConfig
+    }),
   };
 };
